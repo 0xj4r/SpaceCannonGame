11d732598b6e687b3d798382d54f82a011f1d91a
Josh Ransom <josh.bh.ransom@gmail.com>
2014-10-04 19:28:05 -0700

    Add background and cannon to GameScene

--- SpaceCannonGame/GameScene.swift
+++ SpaceCannonGame/GameScene.swift
@@ -9,33 +9,42 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+        // Add the background Image.
+        var background = SKSpriteNode(imageNamed: "Starfield")
+        background.size = CGSizeMake(self.size.width, self.size.height)
+        background.position = CGPointMake(self.size.width/2, self.size.height/2)
+        self.addChild(background)
         
-        self.addChild(myLabel)
+        // Add the main SpriteKit layer.
+        var main = SKNode()
+        self.addChild(main)
+        
+        // Add the canon to the main layer.
+        var cannon = SKSpriteNode(imageNamed: "Cannon")
+        cannon.position = CGPointMake(self.size.width * 0.5, 0.0)
+        main.addChild(cannon)
+        
+        // Add the action sequence for the cannon.
+        var positive = CGFloat(M_PI)
+        var negative = -1 * positive
+        var rotateCannon = SKAction.sequence([SKAction.rotateByAngle(positive, duration: 2.0), SKAction.rotateByAngle(negative, duration: 2.0)])
+        cannon.runAction(SKAction.repeatActionForever(rotateCannon))
+        
     }
     
+    
+    func fireCannon() {
+        var ball = SKSpriteNode(imageNamed: "Ball")
+    }
+      
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
         /* Called when a touch begins */
         
         for touch: AnyObject in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
+            fireCannon()
         }
     }
    
